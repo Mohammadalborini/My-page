7f32f0dae1e3a20ae0b39ee49cb761da41d27966
Mohammad Alnajjad <mohamadalnajjar2000@gmail.com>
2023-02-01 23:58:47 +0300

    My page

--- my page/indexs.php
+++ my page/indexs.php
@@ -13,7 +13,7 @@ session_start();
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="icon" href="img/favicon.png" type="image/png">
-	<title>Mohammad Abduallah</title>
+	<title>Mohammad Abduallah - SE</title>
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="vendors/linericon/style.css">
